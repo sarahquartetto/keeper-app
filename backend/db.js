80eb53f6c241131581964e41957274dbf83c15c9
sarahquartetto <zara.quartz@gmail.com>
2025-08-15 18:30:27 +0200

    Fix __dirname reference error in db.js

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__dirname);
+const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, '.env') });
 
 const { Pool } = pg;
